e5de7e7fead7667fae50962f7de54b1d7ff9d015
jbrennan7651 <103645942+jbrennan7651@users.noreply.github.com>
2022-04-20 10:01:33 -0400

    Finalized Mortgage Calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,44 @@ public class Main {
     //this calculator should give monthly mortgage payments given user inputs
     //annual interest rate should be /12
     public static void main(String[] args) {
+
+        int principal ;
+        double interest ;
+        byte period ;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between $1k - $1M: ");
+        }
         //this asks the user to input their principal loan amount
 
-        System.out.print("Annual Interest Rate: ");
-        double interest = scanner.nextDouble();
-        //this asks the user to input their annual interest
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            interest = scanner.nextDouble();
+            if (interest > 0 && interest <= 100)
+                break;
+            System.out.println("Enter a number from 0 to 100");
+            //this asks the user to input their annual interest
+        }
 
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
-        //most loans will not exceed 127 years so byte is used
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = scanner.nextByte();
+            if (period > 0)
+                break;
+            System.out.println("Enter a number greater than 0");
+            //most loans will not exceed 127 years so byte is used
+        }
+
         double rate = interest/1200;
         // interest is /1200 due to user entry as a % and not as a decimal (3.45 not .0345)
         // this is 12 months * 100% = 1200
+
+
         //the following equation is used for calculation:
         // M = P [r(1+r)^n / (1+r)^n -1]
         //where P is the principal, r is the rate, n is the number of payments
